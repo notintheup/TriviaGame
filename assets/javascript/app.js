392b66241c8690e7149e6931635030ea1a833914
notintheup <brianjandron@gmail.com>
2019-02-18 12:33:51 -0500

    adding js back in after rebuild

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,105 +1,98 @@
 window.onload = function () {
-  $("#startButton").click(function () {
-    $(".btn").hide();
-  })
-  // setup trivia format for game
-  var questionList = [
-    {
-      ask: "What labor force is in highest demand? ",
-      choice: ["Man", "Chesty", "BurnMyHamm", "ShortDon"],
-      ans: false
+  // array of questions, choices and answers
+  var questionList = [{
+      ask: "What % of people 10-29 sleep with their phones? ",
+      choice: ["10%", "30%", "70%", "90%"],
+      ans: 3
     },
     {
-      ask: "What is the birthplace of Sad Sally? ",
-      choice: ["Woman", "Chesty", "BurnMyHammy", "ShortDon"],
-      ans: true
+      ask: "The larger the signature, the larger the self esteem ",
+      choice: ["True", "False"],
+      ans: 0
     },
     {
-      ask: "What is the birthplace of Angry Alan? ",
-      choice: ["Dog", "Chesty", "BurnMyHammer", "ShortDon"],
-      ans: false
+      ask: "People who give you the best advice are the ones with the most problems",
+      choice: ["True", "False"],
+      ans: 0
     },
     {
-      ask: "What is the birthplace of Barrel Bob? ",
-      choice: ["Cat", "Chesty", "BurnMyHamoine", "ShortDon"],
-      ans: false
+      ask: "Creative people tend to get bored easily? ",
+      choice: ["True", "False"],
+      ans: 0
     },
     {
-      ask: (" What is the birthplace of Crazy Jay? "),
-      choice: ["Monkey", "Chesty", "BurnMyHamlet", "TooTimer"],
-      ans: true
+      ask: (" 90% of the things you cannot say in person you prefer? "),
+      choice: ["Burying", "Tell someone else", "Texting", "Singing"],
+      ans: 2
     }
   ]
-  var timer = 15;
-  var clockRunning = false;
+  var number = 30;
   var intervalId;
   var index;
-  var questionSpot = document.getElementsByClassName(".question");
-  var choiceSpot = document.getElementsByClassName(".choiceList");
+  // var questionSpot = document.getElementsByClassName("#question");
+  // var choiceSpot= document.getElementsByClassName("#choiceList");
+
   // Let"s declare some functions
   function loadQuestion() {
-    
     index = Math.floor(Math.random() * questionList.length);
-    
     questionSpot = questionList[index];
-    
-    $(".question").text("question goes here  " + questionList.ask);
-    
-    for (var i = 0; i < questionSpot.length; i++) {
+    $("#question").text("Question: " + questionSpot.ask);
+    for (var i = 0; i < questionList.length; i++) {
       var userChoice = $("<div>");
       userChoice.addClass("")
     }
   }
-  //click function to select answer and outcomes
-$(".answerchoice").on("click", function () {
-	//grab array position from userGuess
-	userGuess = parseInt($(this).attr("data-guessvalue"));
-
-	//correct guess or wrong guess outcomes
-	if (userGuess === pick.answer) {
-		stop();
-		correctCount++;
-		userGuess="";
-		$("#answerblock").html("<p>Correct!</p>");
-		hidepicture();
-
-	} else {
-		stop();
-		wrongCount++;
-		userGuess="";
-		$("#answerblock").html("<p>Wrong! The correct answer is: " + pick.choice[pick.answer] + "</p>");
-		hidepicture();
-	}
-})
-  function start() {
-    if (!clockRunning) {
-      intervalId = setInterval(count, 1000);
-      clockRunning = true;
+  $("#stop").on("click", stop);
+  $("#resume").on("click", run);
+  $("#startButton").on("click", function () {
+    // $("#startButton").hide();
+    loadQuestion();
+    run();
+    for(var i = 0; i < questionList.length; i++) {
+      choiceSpot.push(questionList[i]);
+      consolelog(questionList[i]);
     }
-  }
-  function count() {
-    $(".timeLeft").text(timer);
-    timer--;
-  // kill timer
-    if (timer === 0) {
-      stop();
-      $(".timeLeft").text("<p> Boom! </p>")
-    }
-    var converted = timeConverter(timer);
-    $(".timeLeft").text(converted);
-  }
-  function timeConverter(t) {
-    var minutes = Math.floor(t / 60);
-    var seconds = t - (minutes * 15);
-    if (seconds === 0) {}
-    return seconds;
-  }
-  function stop() {
-    clockRunning = false;
+  })
+
+  function run() {
     clearInterval(intervalId);
-
+    intervalId = setInterval(decrement, 1000);
+    clockRunning = true;
   }
-  loadQuestion();
-  start();
-};
 
+//timer stop
+function stop() {
+	clockRunning = false;
+	clearInterval(intervalId);
+}
+
+    function decrement() {
+      number--;
+      $("#show-number").html("<h2>" + number + "</h2>");
+      if (number === 0) {
+        stop();
+        alert("Loser!");
+      }
+    }
+  //click function to select answer and outcomes
+  $(".answerchoice").on("click", function () {
+    //grab array position from userGuess
+    userGuess = parseInt($(this).attr("data-guessvalue"));
+
+    //correct guess or wrong guess outcomes
+    if (userGuess === pick.answer) {
+      stop();
+      correctCount++;
+      userGuess = "";
+      $("#answerblock").html("<p>Correct!</p>");
+      hidepicture();
+
+    } else {
+      stop();
+      wrongCount++;
+      userGuess = "";
+      $("#answerblock").html("<p>Wrong! The correct answer is: " + pick.choice[pick.answer] + "</p>");
+      hidepicture();
+    }
+  })
+};
