e1307815443f3159a8c58c16e62062df541f94ee
notintheup <brianjandron@gmail.com>
2019-02-23 09:51:33 -0500

    almost done

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,95 +1,113 @@
-window.onload = function () {
-  // array of questions, answers and correctAnswers
-  var questionList = [{
-      question: "What % of people 10-29 sleep with their phones? ",
-      answers: ["10%", "30%", "70%", "90%"],
-      correctAnswer: 3
-    // },
-    // {
-    //   question: "The larger the signature, the larger the self esteem ",
-    //   answers: ["True", "False"],
-    //   correctAnswer: 0
-    // },
-    // {
-    //   question: "People who give you the best advice are the ones with the most problems",
-    //   answers: ["True", "False"],
-    //   correctAnswer: 0
-    // },
-    // {
-    //   question: "Creative people tend to get bored easily? ",
-    //   answers: ["True", "False"],
-    //   correctAnswer: 0
-    // },
-    // {
-    //   question: (" 90% of the things you cannot say in person you prefer? "),
-    //   answers: ["Burying", "Tell someone else", "Texting", "Singing"],
-    //   correctAnswer: 2
-    // }
-  }]
-  var number = 30;
-  var intervalId;
-  var index;
-  // Let"s declare some functions
-  function loadQuestion() {
-    index = Math.floor(Math.random() * questionList.length);
-    questionSpot = questionList[index];
-    //Load question in div
-    $("#question").text("Question: " + questionSpot.question);
-    for (var i = 0; i < questionSpot.answers.length; i++) {
-      var a = $("<div>");
-      a.addClass("#choiceList");
-      a.html(questionSpot.answers[i]);
-      a.attr("click-value", i);
-      $("#choiceList").append(a);
-    }
+//Array of questions with data and game globals
+var questionList = [{
+    question: "What % of people 10-29 sleep with their phones? ",
+    answers: ["10%", "30%", "70%", "90%"],
+    correctAnswer: 3,
+    image: "assets/images/hand.jpg"
+  },
+  {
+    question: "The larger the signature, the larger the self esteem ",
+    answers: ["True", "False"],
+    correctAnswer: 0,
+    image: "assets/images/hand.jpg"
+  },
+  {
+    question: "People who give you the best advice are the ones with the most problems",
+    answers: ["True", "False"],
+    correctAnswer: 0,
+    image: "assets/images/hand.jpg"
+  },
+  {
+    question: "Creative people tend to get bored easily? ",
+    answers: ["True", "False"],
+    correctAnswer: 0,
+    image: "assets/images/hand.jpg"
+  },
+  {
+    question: (" 90% of the things you cannot say in person you prefer? "),
+    answers: ["Burying", "Tell someone else", "Texting", "Singing"],
+    correctAnswer: 2,
+    image: "assets/images/hand.jpg"
   }
-  $("#choiceList").on("click", function (a) {
-    a = parseInt($(questionList).attr("click-value"));
-    if ("a" === questionList.answer) {
+]
+var number = 15;
+var intervalId;
+var index;
+var correct = 0;
+var wrong = 0;
+//Setup the game
+
+function loadQuestion() {
+  index = Math.floor(Math.random() * questionList.length);
+  qOne = questionList[index];
+  $("#question").empty();
+  $("#question").text("Question: " + qOne.question);
+  for (var i = 0; i < qOne.answers.length; i++) {
+    var a = $("<button>");
+    a.addClass("pickButt");
+    a.html(qOne.answers[i]);
+    a.attr("click-value", i);
+    $("#choiceList").append(a);
+  }
+
+  //click function to select answer and outcomes
+  $(".pickButt").on("click", function () {
+    // youPick = questionList.answers;
+    youPick = parseInt($(this).attr("click-value"));
+
+    if (youPick === qOne.correctAnswer) {
       stop();
-      $("#chickenDinner").html("<p>Correct! </p>");
+      correct++;
+      $("#chickenDinner").html("<p>Correct!</p>")
+      reset();
+      // hidepicture();
+
     } else {
       stop();
-      $("#chickenDinner").html("<p>Wrong! </p>");
+      wrong++;
+      $("#chickenDinner").html("<p>Nope!</p>");
+      $("#ding").html(qOne.correctAnswer)
+      reset();
+      // hidepicture();
     }
   })
+}
 
-  $("#stop").on("click", stop);
-  $("#resume").on("click", run);
-  $("#startButton").on("click", function () {
-    loadQuestion();
-    run();
-  })
-  function reset() {
-    $("#reset").hide();
-    $("#answerblock").empty();
-    $("#questionblock").empty();
-    for (var i = 0; i < holder.length; i++) {
-      questionList.push(holder[i]);
-    }
-    runTimer();
-    displayQuestion();
+$("#stop").on("click", stop);
+$("#resume").on("click", run);
+$("#reset").on("click", reset);
+$("#correctCount").html(correct);
+$("#wrongCount").html(wrong);
+$("#startButton").on("click", function () {
+  reset();
+})
 
+function reset() {
+  $("#choiceList").empty();
+  // $("#chickenDinner").empty();
+  $("#question").empty();
+  $("#show-number").empty();
+  run();
+  loadQuestion();
+
+}
+
+function run() {
+  clearInterval(intervalId);
+  intervalId = setInterval(decrement, 1000);
+  clockRunning = true;
+}
+
+function stop() {
+  clockRunning = false;
+  clearInterval(intervalId);
+}
+
+function decrement() {
+  number--;
+  $("#show-number").html("<h2>" + number + "</h2>");
+  if (number === 0) {
+    stop();
+    $(".ding").html(" LOSER! ")
   }
-
-  function run() {
-    clearInterval(intervalId);
-    intervalId = setInterval(decrement, 1000);
-    clockRunning = true;
-  }
-
-  function stop() {
-    clockRunning = false;
-    clearInterval(intervalId);
-  }
-
-  function decrement() {
-    number--;
-    $("#show-number").html("<h2>" + number + "</h2>");
-    if (number === 0) {
-      stop();
-      alert("Loser!");
-    }
-  }
-
-};
+}
